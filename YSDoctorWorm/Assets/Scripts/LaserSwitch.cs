53f32dfd4093f21534ed2ebe7e32c2e0841ba03f
BillySwCodeNinjas <116742021+yehans41@users.noreply.github.com>
2023-05-13 13:51:21 -0500

    worm

---
+++ YSDoctorWorm/Assets/Scripts/LaserSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSwitch : MonoBehaviour
+{
+    private GameObject switchIcon;
+    private AudioSource playBeep;
+
+    public bool lasersAreOff = false;
+
+    private void Start()
+    {
+        switchIcon = this.transform.Find("red").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = true;
+        switchIcon = this.transform.Find("green").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = false;
+
+        playBeep = GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playBeep.Play();
+        lasersAreOff = true;
+}
+
+    public void OnTriggerStay(Collider other)
+    {
+        StopAllCoroutines();
+        switchIcon = this.transform.Find("green").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = true;
+        switchIcon = this.transform.Find("red").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = false;
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        switchIcon = this.transform.Find("red").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = true;
+        switchIcon = this.transform.Find("green").gameObject;
+        switchIcon.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
