6af3e241e6e1b8a371ece33b6e527f381c5b24d9
BillySwCodeNinjas <116742021+yehans41@users.noreply.github.com>
2023-05-18 20:03:41 -0500

    robo 1

--- YSRobomania/Assets/Scripts/EnemyMovement.cs
+++ YSRobomania/Assets/Scripts/EnemyMovement.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float yForce;
-    private RigidBody2D enemyRigidBody;
+    public float xForce;
+    public float xDirection;
+    private Rigidbody2D enemyRigidBody;
 
     void Start()
     {
-        enemyRigidBody = GetComponent<RigidBody2D>();
+        enemyRigidBody = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
         if (transform.position.x <= -8)
         {
-           
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right * xForce);
         }
         if (transform.position.x >= 8)
         {
-           
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left * xForce);
         }
      
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce); // adding a force
+        }
     }
 }
